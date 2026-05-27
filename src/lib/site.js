6bb6118f745dc1c50b3477ac87bde3f05a6a9018
mario <mario.lazok@optimonk.com>
2026-05-27 13:55:39 +0200

    changes

--- src/lib/site.js
+++ src/lib/site.js
@@ -1,51 +1,61 @@
 export const navItems = [
-	{ href: '/#kezdolap', label: 'Kezdőlap' },
-	{ href: '/#rolunk', label: 'Rólunk' },
-	{ href: '/galeria', label: 'Galéria' },
-	{ href: '/blog', label: 'Blog' },
-	{ href: '/videok', label: 'Videók' },
-	{ href: '/#tamogatas', label: 'Támogatás' },
-	{ href: '/#kapcsolat', label: 'Kapcsolat' }
+  { href: "/#kezdolap", label: "Kezdőlap" },
+  { href: "/#rolunk", label: "Rólunk" },
+  { href: "/galeria", label: "Galéria" },
+  { href: "/blog", label: "Blog" },
+  { href: "/videok", label: "Videók" },
+  { href: "/#tamogatas", label: "Támogatás" },
+  { href: "/#kapcsolat", label: "Kapcsolat" },
 ];
 
 export const socialLinks = [
-	{ href: 'https://www.facebook.com/profile.php?id=100066398812314', icon: '/fblogo.png', label: 'Facebook' },
+  {
+    href: "https://www.facebook.com/profile.php?id=100066398812314",
+    icon: "/fblogo.png",
+    label: "Facebook",
+  },
 ];
 
 export const foundation = {
-	name: 'Szentesi Természet- és Vadvédelmi Mentőközpont Alapítvány',
-	shortName: 'Szentesi Vadvédelem',
-	account: '11735043-23537536',
-	tax: '19287283-1-06',
-	phone: '+36 30 633 2900',
-	email: '',
-	address: '6600 Szentes Bocskai u 9.'
+  name: "Szentesi Természet- és Vadvédelmi Mentőközpont Alapítvány",
+  shortName: "Szentesi Vadvédelem",
+  account: "11735043-23537536",
+  tax: "19287283-1-06",
+  phone: "+36 30 633 2900",
+  email: "",
+  address: "6600 Szentes Bocskai u 9.",
 };
 
 export const aboutSections = [
-	{
-		title: 'Tevékenységeink',
-		image: '/galery/unnamed__38_-removebg-preview.png',
-		text: `szaktanácsadás, az állatgondozás módjának bemutatása, megismertetése, a természet és állatvédelemről ismeretterjesztés. A természet- és állatvédelem , gondozás tevékenységek útján korhatár nélkül, az emberek érzelmi, testi, fejlődésének elősegítése, alkotókészségük kibontakoztatása, állóképességük fejlesztése, s az egészséges életmódra nevelésük. A természettudatos életmód, környezetvédelem, természetvédelem népszerűsítése. Mindezekhez intézmények: mentőhely, oktatóközpont. A mentőhely egyben oktató központ, amely nagy hangsúlyt fektet az Állatvédelemre, az óvodás és iskolás gyermekek természet és környezetvédelmi nevelésére, oktatására.A munkánk többrétű, tekintettel arra, hogy nem csak madár mentéssel foglalkozzunk, hanem vadmentéssel is, ezáltal az önkéntes segítőkön kívül a mezőőrök, vadászok Nemzeti Park kollégái is segítik a munkánk.`
-	},
-	{
-		title: 'Egyöttműködés',
-		image: '/galery/unnamed (33).jpg',
-		text: `Az alapítvány összedolgozást folytat Elek város önkormányzatával, Szelezsán György polgármester Úr segíti és támogatja munkánkat. Békéscsaba Megyei Jogú Város Önkormányzatával Szarvas Péter polgármester Úr segíti és támogatja munkánkat. kiemelkedően fontos számukra a természet és élőlényeinek védelme, ezáltal a környezetvédelem. A 2023 . évtől Nagyszénás nagyközség Nyemcsok János polgármester Úrral is megállapodás született, összedolgozás terén. Mindkét városban, valamint a nagyközségben az alapítvány kollégái jelen vannak. Az alapítványnál javarészt a természetből bekerült árva vagy sérült egyedek találhatók meg. Éves szinten közel 700- 800 egyed kerül mentésre Csongrád - Csanád vármegye és Békés vármegye területein. A Békés Vármegyei Kormány Hivatallal valamint a Csongrád Csanád Vármegyei Kormányhivatallal együttműködésben. Fok és fokozottan védett egyedek: Fehér gólyák,Kerecsensólyom, Egerészölyvek, fiatal Parlagi sas, valamint sérülésből adódóan bekerült egyéb fajok: őzek, nyestek, rókák, A Remény kórtermek kialakítását követően: Elesett árva vagy sérült cicákat és időközönként a kutyusokat is tudjuk fogadni, a 2022 évben közel 154 macskát kezeltünk, valamint 12 kölyökkutyának adtunk reményt az életre, egy szerető család megtalálására.`
-	},
-	{
-		title: 'Az alapítvány növekedése',
-		image: '/galery/sas.png',
-		text: `A 2021. - évi indulásunkhoz képest bővült, hiszen árva fiókák, sérült madarak és állatok mentésével kezdtük el a munkánkat. Mára örömmel mondhatjuk, a mentés, kezelés, gyógyítás mellett, nagy hangsúlyt fektetünk a felnövekvő nemzedékre. Az oktató termünkben tanóra keretében tudjuk fogadni az óvodás és iskolás gyerekeket, filmvetítés, előadás ismeretterjesztés téren Szentes városából. Módot és lehetőséget tudunk biztosítani a vidéki intézmények számára, csoportos látogatásra, előre bejelentkezett időpontban. Létrehoztuk a madármentés-lélekmentés programunkat, ezáltal sokkal könnyebben tanulnak a gyerekek az egyes veszélyek által okozott sérülésekről. Nagy hangsúlyt fektetünk az egészséges vagy tartósan sérült gyermekek összedolgozására a fogadj el programunk keretében. A problémákkal küzdő gyermekek részére létrehoztuk az állatsimogató részleget amely terápiás céllal is jól működik, de a felelős állattartásról is sokat tanulhatnak a városi gyerekek`
-	},
-	{
-		title: 'Sikereink',
-		image: '/galery/sas_sikereink.jpeg',
-		text: `Az alapítvány közel két s fél éves munkája során, a kedves támogatóknak köszönhetően, sokat fejlődött. A Szentes város szívében, csodálatos zöld övezetben található alapítvány, sikeresen megtudtuk építeni röpdéinket, kifutóinkat, valamint a fakkos röpdesorokat és amire nagy szükségünk volt a róka kifutót. Elképzeléseink szerint létrehoztuk a remény kórtermeket, az elesett árva cicáknak és kutyáknak valamint a természetben élő, de sérülten beérkező műtét után lábadozó állatoknak. A kezelőket, amiből jelenleg kettő van, fejleszteni tudtuk, eszközökkel, és bútorzattal, igy mind az alapellátás, mind a kezelés feltételei adottak ahhoz, hogy egy sérült állatot megfelelő ellátásban részesítsünk. A pár órás vagy párnapos egyedeknek inkubátort tudtunk beszerezni, ezáltal a megfelelő hőmérsékletet tudjuk a számukra biztosítani. Az oktató terem berendezése is megtörtént, ezáltal mind az iskolás, mind az óvodás gyerekek számára megfelelő oktató teremmel rendelkezik az alapítvány. A 2022. - évben még közel 5.000000 Ft fejlesztést tudtunk véghezvinni , magánszemélyek támogatásával , valamint nyertes pályázatok által. Bethlen Gábor Alapkezelő Zrt: 150.000 Ft Agrárminisztérium: 1.500.000 Ft Herman Ottó Intézet Nonprofit Kft: 500.000 Ft`
-	},
-	{
-		title: 'Elnök',
-		image: '/galery/unnamed (28).jpg',
-		text: `Apavári Györgyné vagyok Szentes városában születtem, 1974.05.18 -án. A természet és élőlényeinek szeretetét és óvását, édesapám és nagyapám által szerettem volna tovább vinni, hiszen nagyon sokat tanultam tőlük, igaz kertesházi gyermek voltam, de tavasztól késő őszig lehetőségem volt kint a természetben, a tisza mellett élni, már gyermekkoromban sok kisállat miatt kerestek meg, akkor még a szüleim örömére, jól emlékszem a napra mikor kaszálás következtében mezei nyuszik sérültek meg, és hozzám kerültek, nagy öröm volt számomra, hogy sikerült őket megmenteni, és persze akkor még gyermekként felnevelni, ugyan úgy mint az árva őzgidát, akinek anyját hétévesen láttam ahogy széttépték sajnos a kutyák. Talán itt dőlt el, mivel is szeretnék foglalkozni, ha felnőtt leszek. Azt, hogy az alapítvány elnöke lehetek egyben az alapítóknak köszönhetem, hiszen mindketten ismernek és tudták, addig küzdök egy állatért, még a szív dobog, mert minden egyes állatnak joga van az élethez, függetlenül attól védett vagy sem.`
-	}
+  {
+    title: "Tevékenységeink",
+    image: "/galery/unnamed__38_-removebg-preview.png",
+    text: `szaktanácsadás, az állatgondozás módjának bemutatása, megismertetése, a természet és állatvédelemről ismeretterjesztés. A természet- és állatvédelem , gondozás tevékenységek útján korhatár nélkül, az emberek érzelmi, testi, fejlődésének elősegítése, alkotókészségük kibontakoztatása, állóképességük fejlesztése, s az egészséges életmódra nevelésük. A természettudatos életmód, környezetvédelem, természetvédelem népszerűsítése. Mindezekhez intézmények: mentőhely, oktatóközpont. A mentőhely egyben oktató központ, amely nagy hangsúlyt fektet az Állatvédelemre, az óvodás és iskolás gyermekek természet és környezetvédelmi nevelésére, oktatására.A munkánk többrétű, tekintettel arra, hogy nem csak madár mentéssel foglalkozzunk, hanem vadmentéssel is, ezáltal az önkéntes segítőkön kívül a mezőőrök, vadászok Nemzeti Park kollégái is segítik a munkánk.`,
+  },
+  {
+    title: "Egyöttműködés",
+    image: "/galery/együtt_sas.jpeg",
+    text: `Az alapítvány együttműködik Békéscsaba Megyei Jogú Város Önkormányzatával, munkánkat Szarvas Péter polgármester úr is segíti és támogatja. Számunkra kiemelten fontos a természet és élővilág védelme, vagyis a környezetvédelem.
+
+Alapítványunk tevékenységének középpontjában a természetből bekerült árva vagy sérült állatok mentése és ellátása áll. Évente mintegy 700–800 egyed kerül hozzánk Csongrád-Csanád és Békés vármegye területéről.
+
+Együttműködünk a Békés Vármegyei és a Csongrád-Csanád Vármegyei Kormányhivatallal, valamint Jász-Nagykun-Szolnok vármegyében a Hortobágyi Nemzeti Parkkal kötött megállapodás keretében is dolgozunk.
+
+Továbbá szoros szakmai kapcsolatban állunk a Szegedi Vadasparkkal és más nemzeti parkokkal is. Vadmentés terén vadászok és mezőőrök közreműködésével segítjük az árva vagy sérült egyedek megmentését.`,
+  },
+  {
+    title: "Az alapítvány növekedése",
+    image: "/galery/sas.png",
+    text: `A 2021. - évi indulásunkhoz képest bővült, hiszen árva fiókák, sérült madarak és állatok mentésével kezdtük el a munkánkat. Mára örömmel mondhatjuk, a mentés, kezelés, gyógyítás mellett, nagy hangsúlyt fektetünk a felnövekvő nemzedékre. Az oktató termünkben tanóra keretében tudjuk fogadni az óvodás és iskolás gyerekeket, filmvetítés, előadás ismeretterjesztés téren Szentes városából. Módot és lehetőséget tudunk biztosítani a vidéki intézmények számára, csoportos látogatásra, előre bejelentkezett időpontban. Létrehoztuk a madármentés-lélekmentés programunkat, ezáltal sokkal könnyebben tanulnak a gyerekek az egyes veszélyek által okozott sérülésekről. Nagy hangsúlyt fektetünk az egészséges vagy tartósan sérült gyermekek összedolgozására a fogadj el programunk keretében. A problémákkal küzdő gyermekek részére létrehoztuk az állatsimogató részleget amely terápiás céllal is jól működik, de a felelős állattartásról is sokat tanulhatnak a városi gyerekek`,
+  },
+  {
+    title: "Sikereink",
+    image: "/galery/sas_sikereink.jpeg",
+    text: `Az alapítvány közel két s fél éves munkája során, a kedves támogatóknak köszönhetően, sokat fejlődött. A Szentes város szívében, csodálatos zöld övezetben található alapítvány, sikeresen megtudtuk építeni röpdéinket, kifutóinkat, valamint a fakkos röpdesorokat és amire nagy szükségünk volt a róka kifutót. Elképzeléseink szerint létrehoztuk a remény kórtermeket, az elesett árva cicáknak és kutyáknak valamint a természetben élő, de sérülten beérkező műtét után lábadozó állatoknak. A kezelőket, amiből jelenleg kettő van, fejleszteni tudtuk, eszközökkel, és bútorzattal, igy mind az alapellátás, mind a kezelés feltételei adottak ahhoz, hogy egy sérült állatot megfelelő ellátásban részesítsünk. A pár órás vagy párnapos egyedeknek inkubátort tudtunk beszerezni, ezáltal a megfelelő hőmérsékletet tudjuk a számukra biztosítani. Az oktató terem berendezése is megtörtént, ezáltal mind az iskolás, mind az óvodás gyerekek számára megfelelő oktató teremmel rendelkezik az alapítvány. A 2022. - évben még közel 5.000000 Ft fejlesztést tudtunk véghezvinni , magánszemélyek támogatásával , valamint nyertes pályázatok által. Bethlen Gábor Alapkezelő Zrt: 150.000 Ft Agrárminisztérium: 1.500.000 Ft Herman Ottó Intézet Nonprofit Kft: 500.000 Ft`,
+  },
+  {
+    title: "Elnök",
+    image: "/galery/unnamed (28).jpg",
+    text: `Apavári Györgyné vagyok Szentes városában születtem, 1974.05.18 -án. A természet és élőlényeinek szeretetét és óvását, édesapám és nagyapám által szerettem volna tovább vinni, hiszen nagyon sokat tanultam tőlük, igaz kertesházi gyermek voltam, de tavasztól késő őszig lehetőségem volt kint a természetben, a tisza mellett élni, már gyermekkoromban sok kisállat miatt kerestek meg, akkor még a szüleim örömére, jól emlékszem a napra mikor kaszálás következtében mezei nyuszik sérültek meg, és hozzám kerültek, nagy öröm volt számomra, hogy sikerült őket megmenteni, és persze akkor még gyermekként felnevelni, ugyan úgy mint az árva őzgidát, akinek anyját hétévesen láttam ahogy széttépték sajnos a kutyák. Talán itt dőlt el, mivel is szeretnék foglalkozni, ha felnőtt leszek. Azt, hogy az alapítvány elnöke lehetek egyben az alapítóknak köszönhetem, hiszen mindketten ismernek és tudták, addig küzdök egy állatért, még a szív dobog, mert minden egyes állatnak joga van az élethez, függetlenül attól védett vagy sem.`,
+  },
 ];
